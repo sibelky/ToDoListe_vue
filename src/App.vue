bbefaa3b7cfadfe791e56a3241881f186d0f88ff
Sibel Kaya <sibelkaya@MBP-von-Sibel.fritz.box>
2025-11-10 22:45:15 +0100

    Initial commit: Vue.js ToDoListe mit v-for-Komponente

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import TodoListe from './components/TodoListe.vue';
+
+export default {
+  name: 'App',
+  components: { TodoListe }
+};
+</script>
+
+<template>
+  <div id="app">
+    <h1>Willkommen zu meiner Vue-App!</h1>
+    <TodoListe />
+  </div>
+</template>
+
+
+<style scoped></style>
---
+++ src/components/TodoListe.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Meine To-Do-Liste</h2>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoListe",
+  data() {
+    return {
+      todos: ["Einkaufen gehen", "Vue lernen", "Projekt starten"]
+    };
+  }
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+}
+li {
+  margin: 5px 0;
+}
+</style>
